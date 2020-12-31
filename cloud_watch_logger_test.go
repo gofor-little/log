a4d0840f3088b6d3a4177085aa27b91cf56edfe0
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2020-12-31 14:17:26 +1100

    feat: better tests
    - Added Dependabot configuation file.
    - Added CI GitHub Action.
    - Added stale GitHub Action for managing stale pull requests and issues.
    - Updated .gitignore.
    - Updated go.mod to go v1.15.
    - Improved CloudWatch logger tests.
    - Updated README.md.

--- cloud_watch_logger_test.go
+++ cloud_watch_logger_test.go
@@ -1,25 +1,37 @@
 package log_test
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/gofor-little/env"
 
 	"github.com/gofor-little/log"
 )
 
 func TestCloudWatchLogger(t *testing.T) {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Region: aws.String("ap-southeast-2"),
-		},
-		Profile: "<PROFILE_NAME>",
-	}))
-
+	var sess *session.Session
 	var err error
+
+	if err := env.Load(".env"); err != nil {
+		t.Log(".env file not found, ignore this if running in CI/CD Pipeline")
+	}
+
+	if env.Get("ENVIRONMENT", "development") == "development" {
+		sess, err = session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				Region: aws.String(env.Get("AWS_REGION", "ap-southeast-2")),
+			},
+			Profile: env.Get("AWS_PROFILE", "default"),
+		})
+	} else {
+		sess, err = session.NewSession()
+	}
+	if err != nil {
+		t.Fatalf("failed to create new session.Session")
+	}
+
 	log.Log, err = log.NewCloudWatchLogger(sess, "CloudWatchLoggerTest", log.Fields{
 		"tag": "cloudWatchLoggerTest",
 	})
@@ -27,13 +39,30 @@ func TestCloudWatchLogger(t *testing.T) {
 		t.Fatalf("failed to create new CloudWatchLogger: %v", err)
 	}
 
-	for i := 0; i < 100; i++ {
-		if err := log.Info(log.Fields{
-			"message": fmt.Sprintf("test info message number: %v", i),
-		}); err != nil {
-			t.Fatalf("failed to write info log: %v", err)
-		}
+	if err := log.Info(log.Fields{
+		"string": "test info string",
+		"bool":   true,
+		"int":    64,
+		"float":  3.14159,
+	}); err != nil {
+		t.Fatalf("failed to write Info log message")
 	}
 
-	time.Sleep(time.Second * 5)
+	if err := log.Error(log.Fields{
+		"string": "test error string",
+		"bool":   true,
+		"int":    64,
+		"float":  3.14159,
+	}); err != nil {
+		t.Fatalf("failed to write Error log message")
+	}
+
+	if err := log.Debug(log.Fields{
+		"string": "test debug string",
+		"bool":   true,
+		"int":    64,
+		"float":  3.14159,
+	}); err != nil {
+		t.Fatalf("failed to write Debug log message")
+	}
 }
